bccf3e0d3e1a2c3f4f45ac3ff4cc1938d97b9861
LPhin <lordphin@gmail.com>
2018-06-17 18:09:14 +0700

    Revert "Merge branch 'todoList'"
    This reverts commit dc4529474c557db1f0e7460e1eef4f9e8ce78713, reversing
    changes made to 3932d340500f0fc34b291d9a56f0c11dc7455df8.

--- miniCalendar/miniCalendar/frmSubTask.cs
+++ miniCalendar/miniCalendar/frmSubTask.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -28,52 +27,3 @@ namespace miniCalendar
 
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace miniCalendar
-{
-    public partial class frmSubTask : UserControl
-    {
-        Dictionary<int, Task> _todoList;
-        int _id;
-        public string _name;
-        public frmSubTask()
-        {
-            InitializeComponent();
-        }
-
-        public frmSubTask(Dictionary<int, Task> _todoList, int _id, string name)
-        {
-            InitializeComponent();
-            this._todoList = _todoList;
-            this._id = _id;
-            lbSubtaskName.Text = name;
-            _name = name;
-        }
-
-        // Lưu status cho subtask khi check hoặc uncheck vào checkbox
-        private void cbIsDone_OnChange(object sender, EventArgs e)
-        {
-            Console.WriteLine(this.TabIndex);
-
-            if (cbIsDone.Checked == true)
-            {
-                _todoList[_id].subTaskStatus[this.TabIndex] = 1;
-            }
-            else
-            {
-                _todoList[_id].subTaskStatus[this.TabIndex] = 0;
-            }
-        }
-    }
-}
->>>>>>> todoList
